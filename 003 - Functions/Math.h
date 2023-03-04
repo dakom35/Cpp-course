703b94ea457f9f07ed11cf4d8738fac93b0077c0
dakom35 <123820119+dakom35@users.noreply.github.com>
2023-03-04 20:39:42 +0100

    Add files via upload

---
+++ 003 - Functions/Math.h
@@ -0,0 +1,18 @@
+/*
+ * Math.h
+ *
+ *  Created on: 10 janv. 2023
+ *      Author: G005872
+ */
+
+#ifndef MATH_H_
+#define MATH_H_
+
+#include <iostream>
+
+int Add(int x, int y);
+void Print(char ch);
+
+
+
+#endif /* MATH_H_ */
