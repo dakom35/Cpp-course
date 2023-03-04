9b49d9e8ca572147273988c8e314dc7e05fabe0a
dakom35 <123820119+dakom35@users.noreply.github.com>
2023-03-04 20:47:08 +0100

    Add files via upload

--- 005 - Uniform initialization/main.cpp
+++ 005 - Uniform initialization/main.cpp
@@ -1,1 +1,52 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 11 janv. 2023
+ *      Author: G005872
+ */
+#include <string>
+#include <iostream>
+
+int main (){
+	int a1 ; // Uninitialized
+	int a2 = 0 ; // copy initialization
+	int a3(5) ; // Direct initialization
+	std::string s1 ; // initialized because the library std does it
+	std::string s2("C++");
+
+	// below are all copy initialization
+	char d1[8] ; // Uninitialized
+	char d2[8] = {'\0'} ;
+	char d3[8] = {'a','b','c','d'} ; //aggregate initialization
+	char d4[8] = {"abcd"} ;
+
+
+
+//	std::cout << "a3 = " << a3 << std::endl ;
+//	std::cout << "s2 = " << s2 << std::endl ;
+//	std::cout << "d2 = " << d2 << std::endl ;
+//	std::cout << "d3 = " << d3 << std::endl ;
+//	std::cout << "d4 = " << d4 << std::endl ;
+
+	// uniform initializations below
+
+	int b1{} ; // value initialization
+	int b3{5} ; // direct initialization
+	char e1[8]{} ;
+	char e2[8]{"Hello"} ;
+	int* p1 = new int{} ;
+	char* p2 = new char[8]{} ; // corresponding ascii symbol = NUL
+
+	std::cout << "b1 = " << b1 << std::endl ;
+	std::cout << "b3 = " << b3 << std::endl ;
+	std::cout << "e1 = " << e1 << std::endl ;
+	std::cout << "e2 = " << e2 << std::endl ;
+	std::cout << "p1 = " << p1 << std::endl ;
+	std::cout << "*p1 = " << *p1 << std::endl ;
+	std::cout << "p2 = " << p2 << std::endl ;
+	std::cout << "*p2 = " << *p2 << std::endl ;
+
+}
+
+
 
